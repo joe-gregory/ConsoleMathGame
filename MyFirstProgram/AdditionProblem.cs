0d11c8724ec9817659ac8d8e80d65c95e507550f
joe-gregory <josephedward2nd@gmail.com>
2023-04-13 10:48:28 -0700

    Add project files.

---
+++ MyFirstProgram/AdditionProblem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    internal class AdditionProblem
+    {
+        public string ProblemText { get; private set; } = string.Empty;
+        public int Number1 { get; private set; }
+        public int Number2 { get; private set; }
+        public int Answer { get; private set; }
+        public int Difficulty { get; private set; }
+
+        public AdditionProblem(int difficulty = 1)
+        {
+            Difficulty = difficulty;
+
+            Random random = new Random();
+
+            switch (Difficulty)
+            {
+                case 1:
+                    Number1 = random.Next(1, 10);
+                    Number2 = random.Next(1, 10);
+                    break;
+                case 2:
+                    Number1 = random.Next(10, 100);
+                    Number2 = random.Next(10, 100);
+                    break;
+                case 3:
+                    Number1 = random.Next(100, 1000);
+                    Number2 = random.Next(100, 1000);
+                    break;
+                case 4:
+                    Number1 = random.Next(1000, 10000);
+                    Number2 = random.Next(1000, 10000);
+                    break;
+                default:
+                    break;
+            }
+            Answer = Number1 + Number2;
+            ProblemText = $"What is the result of adding {Number1} + {Number2}";
+        }
+
+        public bool CheckAnswer(int answer)
+        {
+            if (answer == Answer) return true;
+            else return false;
+        }
+
+    }
+}
---
+++ MyFirstProgram/Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+
+internal class Program
+{
+
+    static string PlayerName = string.Empty;
+    static string ChallengeSelection = string.Empty;
+    static int Difficulty;
+    static int Score = 0;
+
+    const string ChallengeOptionsMenu = @"Choose where to prove them wrong: 
+            A - Addition
+            S - Substraction
+            M - Multiplication
+            D - Division
+            Q - Quit";
+
+    static Dictionary<string, string> ChallengeOptions = new Dictionary<string, string>()
+        {
+            {"A","Addition"},
+            {"S","Substraction" },
+            {"M","Multiplication" },
+            {"D","Division" },
+            {"Q","Quit" }
+        };
+
+    const string DifficultyMenu = @"Select Difficulty:
+            1 - Easy
+            2 - Medium
+            3 - Hard
+            4 - Insane";
+
+    static Dictionary<int, string> DifficultyOptions = new Dictionary<int, string>()
+    {
+        {1, "Easy" },
+        {2, "Medium" },
+        {3, "Hard" },
+        {4, "Insane" }
+    };
+    private static void Main(string[] args)
+    {
+        GetName();
+        ShowIntro();
+        GetChallenge();
+        GetDifficulty();
+        StartGame();
+
+    }
+
+    static void GetName()
+    {
+        bool nameSuccess = false;
+        Console.WriteLine("Name motherfucker, do you have it?");
+        while (!nameSuccess)
+        {
+            var inputName = Console.ReadLine();
+            if (inputName is string)
+            {
+                nameSuccess = true;
+                PlayerName = inputName;
+            }
+            else
+            {
+                Console.WriteLine("I said Name motherfucker, do you have it?");
+            }
+        }
+    }
+
+    static void ShowIntro()
+    {
+        DateTime date = DateTime.UtcNow;
+        Console.WriteLine("----------------------------------------");
+        Console.WriteLine($"Hey foo {PlayerName}. It's {date}. People say you suck at math. Prove them wrong");
+        Console.WriteLine(ChallengeOptionsMenu);
+        Console.WriteLine("----YOUR REPUTATION IS ON THE LINE FOO-----");
+    }
+
+    static void GetChallenge()
+    {
+        bool correctSelection = false;
+
+        while (!correctSelection)
+        {
+            var sel = Console.ReadLine();
+            if (sel is not null)
+            {
+                sel = sel.ToUpper();
+                 if (ChallengeOptions.ContainsKey(sel))
+                    {
+                        correctSelection = true;
+                        ChallengeSelection = sel;
+                    }
+            }
+           
+            else Console.WriteLine("Invalid choice foo");
+        }
+
+        Console.WriteLine($"You have selected the following challenge: {ChallengeOptions[ChallengeSelection]}");
+    }
+
+    static void GetDifficulty()
+    {
+        Console.WriteLine(DifficultyMenu);
+        bool difficultySelectionSuccess = false;
+
+        while (!difficultySelectionSuccess)
+        {
+            var sel = Console.ReadLine();
+            if (int.TryParse(sel, out int trySel))
+            {
+                if (trySel > 0 && trySel < 4)
+                {
+                    difficultySelectionSuccess = true;
+                    Difficulty = trySel;
+                }
+            }
+            if(!difficultySelectionSuccess) Console.WriteLine("Invalid Choice. Select Again.");
+        }
+    }
+
+    static void StartGame()
+    {
+        Console.WriteLine(@$"GAME IS STARTING!
+        {PlayerName} you are playing {ChallengeOptions[ChallengeSelection]} challenge in {DifficultyOptions[Difficulty]} mode.
+        Your current score is {Score}");
+
+    }
+
+    
+}
